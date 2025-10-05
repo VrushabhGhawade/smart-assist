77d4395251803414ede52db7d5539c319cbd5892
Vrushabh Ghawade <vrushabh.ghawade07@gmail.com>
2025-10-05 09:14:11 +0530

    feat: add NgRx store and effects for state management in the application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,16 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptor/auth-interceptor';
 import { errorInterceptor } from './core/interceptor/error-interceptor';
 import { retryInterceptor } from './core/interceptor/retry-interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor,errorInterceptor,retryInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor, retryInterceptor])),
+    provideStore(),
+    provideEffects()
+]
 };
