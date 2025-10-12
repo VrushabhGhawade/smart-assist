4149b5f8af4da45558097d899ed75c550c79135a
Vrushabh Ghawade <vrushabh.ghawade07@gmail.com>
2025-10-12 20:36:35 +0530

    Add new icons and manifest for PWA support
    - Added icon files: icon-72x72.png and icon-96x96.png with appropriate PNG data.
    - Created a new web manifest file (manifest.webmanifest) to define the PWA settings, including app name, display type, and icon configurations for various sizes.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +8,7 @@ import { errorInterceptor } from './core/interceptor/error-interceptor';
 import { retryInterceptor } from './core/interceptor/retry-interceptor';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor, retryInterceptor])),
     provideStore(),
-    provideEffects()
+    provideEffects(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
 ]
 };
